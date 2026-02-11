0583df5f0203ead7cdf8f78c19a5e62abf676dce
Romsm1 <semenkovroman06@gmail.com>
2026-02-11 12:10:57 +0900

    Update models.py

--- lab 2/task 4/models.py
+++ lab 2/task 4/models.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel, Field, field_validator
 import re
 
 
+class User(BaseModel):
+    name: str
+    age: int
+
+
 class Feedback(BaseModel):
     name: str = Field(min_length=2, max_length=50)
     message: str = Field(min_length=10, max_length=500)
@@ -9,11 +14,10 @@ class Feedback(BaseModel):
     @field_validator('message')
     @classmethod
     def validate_message(cls, message: str) -> str:
-        forbidden_bases = ["редиск", "бяк", "козявк"]
+        forbidden_words = ["редиска", "бяка", "козявка"]
         message_lower = message.lower()
-
-        for base in forbidden_bases:
-            if re.search(rf'\b{base}[\w]*\b', message_lower):
+        for forbidden_word in forbidden_words:
+            pattern = rf'\b{forbidden_word[:-2]}[\w]*\b'
+            if re.search(pattern, message_lower):
                 raise ValueError("Использование недопустимых слов")
-
         return message
