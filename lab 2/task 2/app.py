2fea1410fecbb03289643676a4515d6c53076d4d
Romsm1 <semenkovroman06@gmail.com>
2026-02-11 11:52:30 +0900

    Update app.py

--- lab 2/task 2/app.py
+++ lab 2/task 2/app.py
@@ -3,10 +3,22 @@ from models import User
 
 app = FastAPI()
 
+user = User(name="John Doe", age=45)
+
+users_list = [user]
+
+@app.get("/users")
+def get_users():
+    return {
+        "users": users_list,
+        "count": len(users_list)
+    }
+
 @app.post("/user")
 def user_age(user: User):
     return {
         "name": user.name,
         "age": user.age,
         "is_adult": user.age >= 18
+
     }
