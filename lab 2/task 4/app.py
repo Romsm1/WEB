09199d1438b0b8fc8ef6013a93505cd84de5cc1a
Romsm1 <semenkovroman06@gmail.com>
2026-02-11 12:06:31 +0900

    Update app.py

--- lab 2/task 4/app.py
+++ lab 2/task 4/app.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI, HTTPException
-from models import Feedback
+from fastapi import FastAPI
+from models import User, Feedback
 
 app = FastAPI()
 feedbacks = []
+
+user = User(name="John Doe", age=9)
+users_list = [user]
+
+
+@app.get("/users")
+def get_users():
+    return {
+        "users": users_list,
+        "count": len(users_list)
+    }
+
+
+@app.post("/user")
+def check_user_age(user: User):
+    return {
+        "name": user.name,
+        "age": user.age,
+        "is_adult": user.age >= 18
+    }
+
 
 @app.post("/feedback")
 def create_feedback(feedback: Feedback):
@@ -14,9 +35,11 @@ def create_feedback(feedback: Feedback):
         "message": f"Спасибо, {feedback.name}! Ваш отзыв сохранён."
     }
 
+
 @app.get("/feedbacks")
 def get_feedbacks():
     return {
         "feedbacks": feedbacks,
         "count": len(feedbacks)
+
     }
