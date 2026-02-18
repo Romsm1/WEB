c9e0aac5bc6f2b8a5bee0cf1cc004a4e92b341a4
Romsm1 <semenkovroman06@gmail.com>
2026-02-18 09:31:15 +0900

    Delete lab 5 directory

--- lab 5/api/api_v1/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from .books import router as books_router
-from .movies import router as movies_router
-
-router = APIRouter(prefix="/api/v1")
-router.include_router(books_router)
-router.include_router(movies_router)
