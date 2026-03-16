b818a7cf9f117100125780de93fc73626b6d5890
Romsm1 <semenkovroman06@gmail.com>
2026-03-16 16:26:58 +0900

    Update config.py

--- lab 6/core/config.py
+++ lab 6/core/config.py
@@ -1,4 +1,5 @@
 API_TOKENS = frozenset({
-    "aa423",
-    "t0k3n"
+    "552bdd56-a4a6-4b6e-a099-ffb6fb47fe31",
+    "8838e2b5-0476-4b66-bb45-38106c6fa5ab",
+    "b715d678-87ee-4e4e-ae1b-1b7ee83b7c94"
 })
